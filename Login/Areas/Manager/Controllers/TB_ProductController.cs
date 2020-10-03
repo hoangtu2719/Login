4b99edb78c407f02589ee467c760435d05e749ca
Tú Hoàng <hoangtu2719@gmail.com>
2020-10-03 07:01:19 +0700

    First Code

---
+++ Login/Areas/Manager/Controllers/TB_ProductController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Login.Models;
+
+namespace Login.Areas.Manager.Controllers
+{
+    public class TB_ProductController : Controller
+    {
+        private LOGINEntities1 db = new LOGINEntities1();
+
+        // GET: Manager/TB_Product
+        public ActionResult Index()
+        {
+            return View(db.TB_Product.ToList());
+        }
+
+        // GET: Manager/TB_Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TB_Product tB_Product = db.TB_Product.Find(id);
+            if (tB_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tB_Product);
+        }
+
+        // GET: Manager/TB_Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manager/TB_Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdProduct,Name_Product,Img_Product")] TB_Product tB_Product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TB_Product.Add(tB_Product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tB_Product);
+        }
+
+        // GET: Manager/TB_Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TB_Product tB_Product = db.TB_Product.Find(id);
+            if (tB_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tB_Product);
+        }
+
+        // POST: Manager/TB_Product/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdProduct,Name_Product,Img_Product")] TB_Product tB_Product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tB_Product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tB_Product);
+        }
+
+        // GET: Manager/TB_Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TB_Product tB_Product = db.TB_Product.Find(id);
+            if (tB_Product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tB_Product);
+        }
+
+        // POST: Manager/TB_Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TB_Product tB_Product = db.TB_Product.Find(id);
+            db.TB_Product.Remove(tB_Product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Login/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using Login.Models;
+
+namespace Login.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        private LOGINEntities1 login = new LOGINEntities1();
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(FormCollection f)
+        {
+            string staikhoan = f["user_name"].ToString();
+            string smatkhau = f["user_password"].ToString();
+            TB_User user = login.TB_User.SingleOrDefault(n => n.UserName == staikhoan && n.Password == smatkhau);
+            if (user != null)
+            {
+                Session["NguoiDung"] = user;
+                return Redirect(Request.UrlReferrer.ToString());
+            }
+            else if (staikhoan == "")
+            {
+                ViewBag.TBTK = "Nhập tài khoản";
+            }
+            else if (smatkhau == "")
+            {
+                ViewBag.TBMK = "Nhập mật khảu";
+            }
+            else
+            {
+                ViewBag.ThongBao = " <div class=alert - danger>Bạn nhập sai. Vui lòng nhập lại</div>";
+            }
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            Session["NguoiDung"] = null;
+            return Redirect("/User/Login");
+        }
+    }
+}
